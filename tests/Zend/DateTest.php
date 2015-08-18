dfcc68a730533b42a4c236c174b90c3db2e396b8
Vasilis Tsaknis <vtsaknis@travelplanet24.com>
2015-08-18 19:08:11 +0300

    Fix test for en_US locale

--- tests/Zend/DateTest.php
+++ tests/Zend/DateTest.php
@@ -4556,7 +4556,7 @@ class Zend_DateTest extends TestCase
         $date->setYear(2009, $locale)
              ->setMonth(5, $locale)
              ->setDay(31, $locale);
-        $this->assertSame('5/31/09', $date->get(Zend_Date::DATE_SHORT, $locale));
+        $this->assertSame('05/31/2009', $date->get(Zend_Date::DATE_SHORT, $locale));
     }
 
     /**

b101d7125e67ca6ea01a7cc157fa8302e4663859
Vasilis Tsaknis <vtsaknis@travelplanet24.com>
2018-01-08 20:34:51 +0200

    [#B-10312] Zend Db adapter changes

--- library/Zend/Db/Select.php
+++ library/Zend/Db/Select.php
@@ -56,6 +56,8 @@ class Zend_Db_Select
     public const LIMIT_OFFSET   = 'limitoffset';
     public const FOR_UPDATE     = 'forupdate';
     public const FOR_UPDATE_MODE = 'forupdatemode';
+	public const USE_INDEX      = 'useIndex';
+	public const FORCE_INDEX    = 'forceIndex';
 
     // FOR_UPDATE MODES
     public CONST FU_MODE_NOWAIT = 'nowait';
@@ -88,6 +90,8 @@ class Zend_Db_Select
     public const SQL_ON         = 'ON';
     public const SQL_ASC        = 'ASC';
     public const SQL_DESC       = 'DESC';
+	public const SQL_USE_INDEX  = 'USE INDEX';
+	public const SQL_FORCE_INDEX = 'FORCE INDEX';
 
     public const REGEX_COLUMN_EXPR       = '/^([\w]*\s*\(([^\(\)]|(?1))*\))$/';
     public const REGEX_COLUMN_EXPR_ORDER = '/^([\w]+\s*\(([^\(\)]|(?1))*\))$/';
@@ -140,6 +144,8 @@ class Zend_Db_Select
         self::GROUP        => [],
         self::HAVING       => [],
         self::ORDER        => [],
+        self::USE_INDEX    => [],
+        self::FORCE_INDEX  => [],
         self::LIMIT_COUNT  => null,
         self::LIMIT_OFFSET => null,
         self::FOR_UPDATE   => false,
@@ -1204,13 +1210,10 @@ class Zend_Db_Select
                 $tmp .= ' ' . self::SQL_FORCE_INDEX . '(' . implode(',', $this->_parts[self::FORCE_INDEX]) . ')';
                 unset($this->_parts[self::FORCE_INDEX]);
             }
-            if (!empty($this->_parts[self::IGNORE_INDEX])) {
-                $tmp .= ' ' . self::SQL_IGNORE_INDEX . '(' . implode(',', $this->_parts[self::IGNORE_INDEX]) . ')';
-                unset($this->_parts[self::IGNORE_INDEX]);
-            }
 
             // Add join conditions (if applicable)
             if (!empty($from) && ! empty($table['joinCondition'])) {
+
                 $tmp .= ' ' . self::SQL_ON . ' ' . $table['joinCondition'];
             }
 
@@ -1439,48 +1442,39 @@ class Zend_Db_Select
     }
 
     /**
-     * Specify index to use. Works only on mysql
+     * Specify index to use
      *
-     * @param string $index
      * @return Zend_Db_Select
      */
     public function useIndex($index)
     {
-        if (!is_array($index)) {
-            $index = array($index);
+        if(empty($this->_parts[self::FORCE_INDEX])) {
+            if(!is_array($index)) {
+                $index = array($index);
+            }
+            $this->_parts[self::USE_INDEX] = $index;
+            return $this;
+        } else {
+            throw new Zend_Db_Select_Exception("Cannot use 'USE INDEX' in the same query as 'FORCE INDEX'");
         }
-        $this->_parts[self::USE_INDEX] = $index;
-        return $this;
     }
 
     /**
-     * Force index. Works only on mysql
+     * Force index to use
      *
-     * @param string $index
      * @return Zend_Db_Select
      */
     public function forceIndex($index)
     {
-        if (!is_array($index)) {
-            $index = array($index);
+        if(empty($this->_parts[self::USE_INDEX])) {
+            if(!is_array($index)) {
+                $index = array($index);
+            }
+            $this->_parts[self::FORCE_INDEX] = $index;
+            return $this;
+        } else {
+            throw new Zend_Db_Select_Exception("Cannot use 'FORCE INDEX' in the same query as 'USE INDEX'");
         }
-        $this->_parts[self::FORCE_INDEX] = $index;
-        return $this;
-    }
-
-    /**
-     * Ignore index. Works only on mysql
-     *
-     * @param string $index
-     * @return Zend_Db_Select
-     */
-    public function ignoreIndex($index)
-    {
-        if (!is_array($index)) {
-            $index = array($index);
-        }
-        $this->_parts[self::IGNORE_INDEX] = $index;
-        return $this;
     }
 
 }

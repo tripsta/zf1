901f3809b446a4b8c30706feb5caa64df6b9db9c
antonis179 <antonis179@gmail.com>
2018-01-10 16:04:19 +0200

    Yet another test fixed! (TP added to available locales)

--- library/Zend/Locale.php
+++ library/Zend/Locale.php
@@ -723,6 +723,7 @@ class Zend_Locale
         'tr'          => true,
         'tr_CY'       => true,
         'tr_TR'       => true,
+        'tp'          => true,
         'ts'          => true,
         'ts_ZA'       => true,
         'twq'         => true,

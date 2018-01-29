3ef40520bbfbbac0cbba881ade40b434c11aee98
antonis179 <antonis179@gmail.com>
2018-01-29 12:16:23 +0200

    Muted tempnam method to quietly write in the temp directory

--- library/Zend/Cache/Backend.php
+++ library/Zend/Cache/Backend.php
@@ -200,7 +200,7 @@ class Zend_Cache_Backend
             }
         }
         // Attemp to detect by creating a temporary file
-        $tempFile = tempnam(md5(uniqid(rand(), TRUE)), '');
+        $tempFile = @tempnam(md5(uniqid(rand(), TRUE)), '');
         if ($tempFile) {
             $dir = realpath(dirname($tempFile));
             unlink($tempFile);
